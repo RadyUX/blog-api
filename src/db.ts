20fbf9bb574360fb410eb06a2fe08b20e60ce571
rady <rafaele.sinaguglia@gmail.com>
2024-08-23 06:43:38 +0200

    set timeout

--- src/db.ts
+++ src/db.ts
@@ -2,19 +2,23 @@ import dbConfig from "./config/db.config";
 import mysql from "mysql2"
 
 const pool = mysql.createPool({
-    host: process.env.HOST,
-    user:  process.env.USER,
-    password:  process.env.PASSWORD,
-    database:  process.env.DB,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-  });
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DB,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 20000 // 10 seconds
+});
 
- pool.getConnection(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
-  
+pool.getConnection(function(err) {
+  if (err) {
+      console.error('Error connecting to the database:', err);
+      throw err;
+  }
+  console.log("Connected!");
+});
+
 
   export default pool
